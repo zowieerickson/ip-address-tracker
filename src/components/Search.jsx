09c56304f31b70a81530a73a707c4e17f507c7d1
Zowie Erickson <zowie.erickson@gmail.com>
2023-07-12 08:16:53 -0400

    add svg to end of search input, adjust search input height with padding

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -13,13 +13,19 @@ export default function Search() {
     }
 
     return (
-        <input 
-        onKeyDown={handleKeyDown}
-        type="search" 
-        className="search" 
-        placeholder="Search for any IP address or domain" 
-        name="" 
-        id="" 
-        />
+        <div className="search-wrapper">
+            <input 
+            onKeyDown={handleKeyDown}
+            type="search" 
+            className="search" 
+            placeholder="Search for any IP address or domain" 
+            name="" 
+            id="" 
+            />
+            <svg xmlns="http://www.w3.org/2000/svg" width="58" height="58" viewBox="0 0 58 58" fill="none">
+<path d="M0 0H43C51.2843 0 58 6.71573 58 15V43C58 51.2843 51.2843 58 43 58H0V0Z" fill="black"/>
+<path d="M26 23L32 29L26 35" stroke="white" strokeWidth="3"/>
+</svg>
+        </div>
     )
 }
