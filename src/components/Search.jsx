f21ce887335eae2035635d951f4fab882bf13653
Zowie Erickson <zowie.erickson@gmail.com>
2023-07-11 20:27:14 -0400

    add starter onKeyDown handler to search input to listen when the user presses Enter

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,7 +2,24 @@ import '../styles/search.css'
 
 export default function Search() {
 
+    function handleKeyDown(e) {
+        // If the user presses the "Enter" key on the keyboard
+        if (e.key === "Enter") {
+            // Cancel the default action, if needed
+            e.preventDefault();
+            // Trigger
+            console.log("ayo")
+        }
+    }
+
     return (
-        <input type="search" className="search" placeholder="Search for any IP address or domain" name="" id="" />
+        <input 
+        onKeyDown={handleKeyDown}
+        type="search" 
+        className="search" 
+        placeholder="Search for any IP address or domain" 
+        name="" 
+        id="" 
+        />
     )
 }
