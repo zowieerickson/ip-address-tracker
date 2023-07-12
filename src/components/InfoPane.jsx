d3df3578cf628f8637d1257c52131f3012926347
Zowie Erickson <zowie.erickson@gmail.com>
2023-07-12 16:45:32 -0400

    add vertical dividers between info pane sections

--- src/components/InfoPane.jsx
+++ src/components/InfoPane.jsx
@@ -10,22 +10,25 @@ export default function InfoPane() {
                     <p>192.212.174.101</p>
                 </div>
             </section>
+            <div className="vl"></div>
             <section className="info-item ">
                 <div className="info-details">
-                    <h2>IP Address</h2>
-                    <p>192.212.174.101</p>
+                    <h2>Location</h2>
+                    <p>Brooklyn, NY 10001</p>
                 </div>
             </section>
+            <div className="vl"></div>
             <section className="info-item ">
                 <div className="info-details">
-                    <h2>IP Address</h2>
-                    <p>192.212.174.101</p>
+                    <h2>Timezone</h2>
+                    <p>UTC -05:00</p>
                 </div>
             </section>
+            <div className="vl"></div>
             <section className="info-item ">
                 <div className="info-details">
-                    <h2>IP Address</h2>
-                    <p>192.212.174.101</p>
+                    <h2>ISP</h2>
+                    <p>SpaceX Starlink</p>
                 </div>
             </section>
         </section>
