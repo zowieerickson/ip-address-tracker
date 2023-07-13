72ad8d65ec9f158381fa2505e8ac9e5fdc7303af
Zowie Erickson <zowie.erickson@gmail.com>
2023-07-13 19:06:06 -0400

    add leaflet and create a starter map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,6 +1,16 @@
+import '../styles/map.css'
+import 'leaflet/dist/leaflet.css'
+import { MapContainer, TileLayer, useMap } from 'react-leaflet'
+
 export default function Map() {
 
     return (
-        <h1>Map</h1>
+        // <h1>Map</h1>
+        <MapContainer center={[51.505, -0.09]} zoom={13}>
+              <TileLayer
+    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  />
+        </MapContainer>
     )
 }
