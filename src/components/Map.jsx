b786a4b10d64502e019ebc21f9e5e5d3f046baef
Zowie Erickson <zowie.erickson@gmail.com>
2023-07-14 08:46:59 -0400

    update leaflet map appearance using  mapbox tilelayer

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,16 +1,28 @@
 import '../styles/map.css'
 import 'leaflet/dist/leaflet.css'
-import { MapContainer, TileLayer, useMap } from 'react-leaflet'
+import { MapContainer as LeafletMap,  Marker, Popup, TileLayer, useMap } from 'react-leaflet'
+
+// GET ONE in https://docs.mapbox.com/help/how-mapbox-works/access-tokens/";
+const ACCESS_TOKEN = "pk.eyJ1Ijoiem93aWVoZXlyZWFsIiwiYSI6ImNsazJqc29lZjBlMGszbnBibWtvMWtyN3oifQ.SaCH76qUhp8l1BnUkyR5CQ";
+
+const URL = `https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token=${ACCESS_TOKEN}`;
+const ATTRIBUTION =
+  'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>';
+
+const TOWER_LOCATION = [43.385807, -8.406524];
 
 export default function Map() {
 
     return (
-        // <h1>Map</h1>
-        <MapContainer center={[51.505, -0.09]} zoom={13}>
-              <TileLayer
-    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-  />
-        </MapContainer>
+        <LeafletMap center={TOWER_LOCATION} zoom={16}>
+            <TileLayer url={URL} attribution={ATTRIBUTION} />
+            <Marker position={TOWER_LOCATION}>
+            <Popup>
+                <b>Tower of Hercules</b>
+                <br />
+                UNESCO World Heritage site
+            </Popup>
+            </Marker>
+        </LeafletMap>
     )
 }
