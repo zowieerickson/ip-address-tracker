2561823618f625fbf668b069102bfcae6b9ca45d
Zowie Erickson <zowie.erickson@gmail.com>
2023-07-14 17:37:03 -0400

    move API call into DataWrapper component, and reorganize API calls to be nested as data was returning undefined to async code

---
+++ src/components/DataWrapper.jsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+
+export default function DataWrapper() {
+    const [initialIp, setInitialIp] = useState({});
+    const [data, setData] = useState({})
+    const [error, setError] = useState({});
+
+  
+    useEffect(() => {
+      fetch(`https://api.ipify.org?format=json`)
+        .then(response => response.json())
+        .then(json => {
+            setInitialIp(json);
+            // Make the second API call here, once the initialIp state is updated
+            fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_wlkmwxC4IWSmbVuwksl7DqRyieXon&ipAddress=${json.ip}`)
+              .then(response => response.json())
+              .then(json => setData(json))
+              .catch(error => setError(error));
+          })
+          .catch(error => setError(error));
+      }, []);
+
+      
+      console.log(error)
+      console.log(initialIp)
+      console.log(data)
+
+      return (
+        <>
+          {data ? <h2 className="ip">{JSON.stringify(data.ip, null, 2)}</h2> : 'Loading...'}
+        </>
+      )
+}
