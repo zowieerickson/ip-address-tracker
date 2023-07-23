cff34cfac4e69c82fc80e3ea5383c1676e1f7664
Zowie Erickson <zowie.erickson@gmail.com>
2023-07-23 11:03:54 -0400

    Update API call in serverless function

--- netlify/functions/getIPAddress.js
+++ netlify/functions/getIPAddress.js
@@ -1,21 +1,36 @@
 // netlify/functions/getIPAddress.js
 
+const axios = require('axios');
+
 exports.handler = async (event, context) => {
-    // Your API key should be stored as an environment variable on Netlify
-    const apiKey = process.env.IP_API_KEY;
-  
-    // Your logic to fetch the IP address information using your API key
-    // For example, if you're using axios:
-    // const axios = require('axios');
-    // const ipAddress = event.queryStringParameters.ipAddress;
-    // const response = await axios.get(`https://api.example.com/ip-info/${ipAddress}?apiKey=${apiKey}`);
-    // const data = response.data;
-  
-    // Replace the example code above with the logic to fetch IP information using your chosen API library
-  
+  // API key should be stored as an environment variable on Netlify
+  const apiKey = process.env.IP_API_KEY;
+
+  try {
+    // Get the ipAddress from the queryStringParameters in the event
+    const ipAddress = event.queryStringParameters.ipAddress;
+
+    // Make the API request to the IP Geolocation API using Axios
+    const response = await axios.get(
+      `https://geo.ipify.org/api/v2/country,city?apiKey=${apiKey}&ipAddress=${ipAddress}`
+    );
+
+    // Extract the data from the response
+    const data = response.data;
+
     // Return the result to the client
     return {
       statusCode: 200,
       body: JSON.stringify(data),
     };
-  };
+  } catch (error) {
+    // Handle any errors that occurred during the API request
+    console.error('Error fetching IP information:', error);
+
+    // Return an error response to the client
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Failed to fetch IP information' }),
+    };
+  }
+};
