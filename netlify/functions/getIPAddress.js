868782d2700008b38494a445c83f0e2beb6a3772
Zowie Erickson <zowie.erickson@gmail.com>
2023-07-23 11:30:27 -0400

    Update serverless function to handle both IP address and domain queries correctly. Before, it was only handling IP address queries, so domain queries would fail

--- netlify/functions/getIPAddress.js
+++ netlify/functions/getIPAddress.js
@@ -7,22 +7,45 @@ exports.handler = async (event, context) => {
   const apiKey = process.env.IP_API_KEY;
 
   try {
-    // Get the ipAddress from the queryStringParameters in the event
+    // Check if the request has the 'ipAddress' or 'domain' query parameter
     const ipAddress = event.queryStringParameters.ipAddress;
+    const domain = event.queryStringParameters.domain;
 
-    // Make the API request to the IP Geolocation API using Axios
-    const response = await axios.get(
-      `https://geo.ipify.org/api/v2/country,city?apiKey=${apiKey}&ipAddress=${ipAddress}`
-    );
+    if (ipAddress) {
+      // Make the API request to the IP Geolocation API with the 'ipAddress' parameter
+      const response = await axios.get(
+        `https://geo.ipify.org/api/v2/country,city?apiKey=${apiKey}&ipAddress=${ipAddress}`
+      );
 
-    // Extract the data from the response
-    const data = response.data;
+      // Extract the data from the response
+      const data = response.data;
 
-    // Return the result to the client
-    return {
-      statusCode: 200,
-      body: JSON.stringify(data),
-    };
+      // Return the result to the client
+      return {
+        statusCode: 200,
+        body: JSON.stringify(data),
+      };
+    } else if (domain) {
+      // Make the API request to the IP Geolocation API with the 'domain' parameter
+      const response = await axios.get(
+        `https://geo.ipify.org/api/v2/country,city?apiKey=${apiKey}&domain=${domain}`
+      );
+
+      // Extract the data from the response
+      const data = response.data;
+
+      // Return the result to the client
+      return {
+        statusCode: 200,
+        body: JSON.stringify(data),
+      };
+    } else {
+      // Return an error response if neither 'ipAddress' nor 'domain' query parameter is provided
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ error: 'Invalid query parameters' }),
+      };
+    }
   } catch (error) {
     // Handle any errors that occurred during the API request
     console.error('Error fetching IP information:', error);
