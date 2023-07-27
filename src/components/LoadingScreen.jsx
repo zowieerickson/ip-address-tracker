1ace6ff466f7945a1646a0e6757b9392bca599b4
Zowie Erickson <zowie.erickson@gmail.com>
2023-07-27 08:42:32 -0400

    Add icon to loading screen

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -1,9 +1,11 @@
 import '../styles/loading-screen.css'
+import locationIcon from "../../public/icons8-location-96.png"
 
 export default function LoadingScreen() {
     return (
         <div className="loading-screen">
             <h1 className="loading-screen-copy">IP Address Tracker</h1> 
+            <img className="loading-screen-icon" src={locationIcon} alt="" />
         </div>
     )
 }
