657bd85b8218880589b5dc703ee5b82b39a84054
Zowie Erickson <zowie.erickson@gmail.com>
2023-07-27 13:09:30 -0400

    Remove icon on loading screen

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -1,12 +1,10 @@
 import '../styles/loading-screen.css'
-import locationIcon from "../../public/icons8-location-96.png"
 
 export default function LoadingScreen() {
     return (
         <div className="loading-screen">
             <h1 className="loading-copy">IP Address Tracker</h1> 
             <span className="loading-bar"></span>
-            <img className="loading-icon" src={locationIcon} alt="" />
         </div>
     )
 }
