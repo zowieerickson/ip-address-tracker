cb40c1def08f98cc252d77642880f10a23d62872
Zowie Erickson <zowie.erickson@gmail.com>
2023-07-27 13:02:15 -0400

    Add loading bar animation

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -4,8 +4,9 @@ import locationIcon from "../../public/icons8-location-96.png"
 export default function LoadingScreen() {
     return (
         <div className="loading-screen">
-            <h1 className="loading-screen-copy">IP Address Tracker</h1> 
-            <img className="loading-screen-icon" src={locationIcon} alt="" />
+            <h1 className="loading-copy">IP Address Tracker</h1> 
+            <span className="loading-bar"></span>
+            <img className="loading-icon" src={locationIcon} alt="" />
         </div>
     )
 }
